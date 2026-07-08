f03e3d8c8cd30364dae95c822b5cc7de461ceb0d
chiru-1992 <chirugurung730@gmail.com>
2026-07-08 18:29:33 +0530

    Update players.js

--- players.js
+++ players.js
@@ -1,8 +1,7 @@
-// players.js
-
 const SECRET_CODE = "NINJA TURTLE";
 
 const tickets = [
+
 {
     ticket: 1,
     buyer: "Priyanka",
@@ -19,6 +18,7 @@ const tickets = [
         image: "images/morocco/achraf_hakimi.jpg"
     }
 },
+
 {
     ticket: 2,
     buyer: "Niraj",
@@ -35,6 +35,7 @@ const tickets = [
         image: "images/morocco/noussair_mazraoui.jpg"
     }
 },
+
 {
     ticket: 3,
     buyer: "Niraj",
@@ -46,9 +47,347 @@ const tickets = [
     },
 
     morocco: {
-        name: "Zakaria El Ouahdi",
+        name: "Zakaria El Ouhdi",
         number: "13",
         image: "images/morocco/zakaria_el_ouhdi.jpg"
     }
+},
+
+{
+    ticket: 4,
+    buyer: "Karan",
+
+    france: {
+        name: "Michael Olise",
+        number: "11",
+        image: "images/france/michael_olise.jpg"
+    },
+
+    morocco: {
+        name: "Issa Diop",
+        number: "14",
+        image: "images/morocco/issa_diop.jpg"
+    }
+},
+
+{
+    ticket: 5,
+    buyer: "Nitu",
+
+    france: {
+        name: "Bradley Barcola",
+        number: "12",
+        image: "images/france/bradley_barcola.jpg"
+    },
+
+    morocco: {
+        name: "Chadi Riad",
+        number: "18",
+        image: "images/morocco/chadi_riad.jpg"
+    }
+},
+
+{
+    ticket: 6,
+    buyer: "Ashwin",
+
+    france: {
+        name: "Désiré Doué",
+        number: "20",
+        image: "images/france/desire_doue.jpg"
+    },
+
+    morocco: {
+        name: "Youssef Belammari",
+        number: "19",
+        image: "images/morocco/youssef_belammari.jpg"
+    }
+},
+    {
+    ticket: 7,
+    buyer: "Diwakar",
+
+    france: {
+        name: "Jean-Philippe Mateta",
+        number: "22",
+        image: "images/france/jean_philippe_mateta.jpg"
+    },
+
+    morocco: {
+        name: "Redouane Halhal",
+        number: "25",
+        image: "images/morocco/redouane_halhal.jpg"
+    }
+},
+
+{
+    ticket: 8,
+    buyer: "Ashwin",
+
+    france: {
+        name: "Rayan Cherki",
+        number: "24",
+        image: "images/france/rayan_cherki.jpg"
+    },
+
+    morocco: {
+        name: "Anass Salah-Eddine",
+        number: "26",
+        image: "images/morocco/anass_salah_eddine.jpg"
+    }
+},
+
+{
+    ticket: 9,
+    buyer: "Nitu",
+
+    france: {
+        name: "Maghnes Akliouche",
+        number: "25",
+        image: "images/france/maghnes_akliouche.jpg"
+    },
+
+    morocco: {
+        name: "Marwane Saadane",
+        number: "5",
+        image: "images/morocco/marwane_saadane.jpg"
+    }
+},
+
+{
+    ticket: 10,
+    buyer: "Pranika",
+
+    france: {
+        name: "N'Golo Kanté",
+        number: "13",
+        image: "images/france/ngolo_kante.jpg"
+    },
+
+    morocco: {
+        name: "Sofyan Amrabat",
+        number: "4",
+        image: "images/morocco/sofyan_amrabat.jpg"
+    }
+},
+
+{
+    ticket: 11,
+    buyer: "Kamna",
+
+    france: {
+        name: "Adrien Rabiot",
+        number: "14",
+        image: "images/france/adrien_rabiot.jpg"
+    },
+
+    morocco: {
+        name: "Ayoub Ouaddi",
+        number: "6",
+        image: "images/morocco/ayoub_ouaddi.jpg"
+    }
+},
+
+{
+    ticket: 12,
+    buyer: "Karan",
+
+    france: {
+        name: "Aurélien Tchouaméni",
+        number: "8",
+        image: "images/france/aurelien_tchouameni.jpg"
+    },
+
+    morocco: {
+        name: "Chemsdine Talbi",
+        number: "7",
+        image: "images/morocco/chemsdine_talbi.jpg"
+    }
+},
+    {
+    ticket: 13,
+    buyer: "Nikita",
+
+    france: {
+        name: "Manu Koné",
+        number: "6",
+        image: "images/france/manu_kone.jpg"
+    },
+
+    morocco: {
+        name: "Azzedine Punahi",
+        number: "8",
+        image: "images/morocco/azzedine_punahi.jpg"
+    }
+},
+
+{
+    ticket: 14,
+    buyer: "Kamna",
+
+    france: {
+        name: "Warren Zaïre-Emery",
+        number: "18",
+        image: "images/france/warren_zaire_emery.jpg"
+    },
+
+    morocco: {
+        name: "Ismael Saibari",
+        number: "11",
+        image: "images/morocco/ismael_saibari.jpg"
+    }
+},
+
+{
+    ticket: 15,
+    buyer: "Sishir",
+
+    france: {
+        name: "Malo Gusto",
+        number: "2",
+        image: "images/france/malo_gusto.jpg"
+    },
+
+    morocco: {
+        name: "Samir El Mourabet",
+        number: "15",
+        image: "images/morocco/samir_el_mourabet.jpg"
+    }
+},
+
+{
+    ticket: 16,
+    buyer: "Manisha",
+
+    france: {
+        name: "Lucas Digne",
+        number: "3",
+        image: "images/france/lucas_digne.jpg"
+    },
+
+    morocco: {
+        name: "Gessime Yassine",
+        number: "16",
+        image: "images/morocco/gessime_yassine.jpg"
+    }
+},
+
+{
+    ticket: 17,
+    buyer: "Aditya",
+
+    france: {
+        name: "Dayot Upamecano",
+        number: "4",
+        image: "images/france/dayot_upamecano.jpg"
+    },
+
+    morocco: {
+        name: "Bilal El Khannouss",
+        number: "23",
+        image: "images/morocco/bilal_el_khannouss.jpg"
+    }
+},
+
+{
+    ticket: 18,
+    buyer: "Nikita",
+
+    france: {
+        name: "Jules Koundé",
+        number: "5",
+        image: "images/france/jules_kounde.jpg"
+    },
+
+    morocco: {
+        name: "Neil El Aynaoui",
+        number: "24",
+        image: "images/morocco/neil_el_aynaoui.jpg"
+    }
+},
+    {
+    ticket: 19,
+    buyer: "Aditya",
+
+    france: {
+        name: "Ibrahima Konaté",
+        number: "15",
+        image: "images/france/ibrahima_konate.jpg"
+    },
+
+    morocco: {
+        name: "Brahim Diaz",
+        number: "10",
+        image: "images/morocco/brahim_diaz.jpg"
+    }
+},
+
+{
+    ticket: 20,
+    buyer: "Pranika",
+
+    france: {
+        name: "William Saliba",
+        number: "17",
+        image: "images/france/William_saliba.jpg"
+    },
+
+    morocco: {
+        name: "Soufiane Rahimi",
+        number: "9",
+        image: "images/morocco/soufiane_rahimi.jpg"
+    }
+},
+
+{
+    ticket: 21,
+    buyer: "Manisha",
+
+    france: {
+        name: "Theo Hernández",
+        number: "19",
+        image: "images/france/theo_hernandez.jpg"
+    },
+
+    morocco: {
+        name: "Ayoub El Kaabi",
+        number: "20",
+        image: "images/morocco/ayoub_el_kaabi.jpg"
+    }
+},
+
+{
+    ticket: 22,
+    buyer: "Satish",
+
+    france: {
+        name: "Lucas Hernández",
+        number: "21",
+        image: "images/france/lucas_hernandez.jpg"
+    },
+
+    morocco: {
+        name: "Ayoube Ammaimouni",
+        number: "21",
+        image: "images/morocco/ayoube_ammaimouni.jpg"
+    }
+},
+
+{
+    ticket: 23,
+    buyer: "Priyanka",
+
+    france: {
+        name: "Maxence Lacroix",
+        number: "26",
+        image: "images/france/maxence_lacroix.jpg"
+    },
+
+    morocco: {
+        name: "Amine Sbai",
+        number: "22",
+        image: "images/morocco/amine_sbai.jpg"
+    }
 }
+
 ];
